3d1a7b1c9080419112cbab102b4f944fca23be58
you@example.com <you@example.com>
2025-11-14 09:10:57 +0200

    js scripts gatavs bez history

---
+++ app.js
@@ -0,0 +1,33 @@
+let numberToGuess = Math.floor(Math.random() * 100) + 1;
+let guessCounter = 0;
+
+const numberInput = document.getElementById("number");
+const submitButton = document.getElementById("submit");
+const responseText = document.getElementById("response");
+
+submitButton.addEventListener("click", handleClick)
+
+function handleClick() {
+    let numberGuess = numberInput.value;
+    guessCounter++;
+
+    switch (true) {
+
+        case numberGuess < 1:
+            responseText.textContent = "Please enter a valid number between 1 and 100";
+            break;
+        case numberGuess > 100:
+            responseText.textContent = "Please enter a valid number between 1 and 100";
+            break;
+        case numberGuess > numberToGuess:
+            responseText.textContent = "Too high! Try again.";
+            break;
+        case numberGuess < numberToGuess:
+            responseText.textContent = "Too low! Try again.";
+            break;
+        case numberGuess == numberToGuess:
+            responseText.textContent = `Congratulations! You guessed the number in ${guessCounter} attempts`; 
+            break;
+    }
+
+}
